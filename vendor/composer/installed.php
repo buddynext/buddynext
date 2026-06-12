e5becb440ac542242cdad69dd6865d0809bb7aae
Varun Kumar Dubey <vapvarun@gmail.com>
2026-06-12 13:06:52 +0530

    Bundle composer vendor directory so installs require no build step

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wbcom-designs/buddynext',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2fea39a945105dbae0cab35421a502546cfd6400',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wbcom-designs/buddynext' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2fea39a945105dbae0cab35421a502546cfd6400',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
